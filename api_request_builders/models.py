233889370b4c696b39c3be567dce33ef4502d55b
Mertcan Seğmen <mertcan.segmen@hotmail.com>
2025-01-07 23:36:01 +0300

    remove api_request_builders as they are now in humanized_api

--- api_request_builders/models.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-from pydantic import BaseModel
-from tools.api_requestor import APIRequest
-
-class ApiRequestBuilderResponse(BaseModel):
-    result: str
-    message: Optional[str] = None
-    api_request: Optional['APIRequest'] = None
